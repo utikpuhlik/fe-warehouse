99454df81641ea7504606c0e5da0018e58ebf043
Vadym Tatarinov <utikpuhlik@protonmail.ch>
2025-06-06 12:50:56 +0200

    fix(pricing): base url

--- app/lib/apis/pricingApi.ts
+++ app/lib/apis/pricingApi.ts
@@ -1,7 +1,8 @@
-import {BASE_URL} from "@/app/lib/config/config";
+const BASE_URL: string = "https://api-tcf.eucalytics.uk"
 
 export async function fetchPriceList(price_type: string, ext: string): Promise<Response> {
     const url = `${BASE_URL}/price/${price_type}?ext=${ext}`;
+    console.log(url)
     const res = await fetch(url);
 
     if (!res.ok) {
