7062ff312bea12b4ae7f759086325af730f82c1f
Vadym Tatarinov <utikpuhlik@protonmail.ch>
2025-06-09 22:35:13 +0200

    refactor: auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "@/app/ui/catalogue/login/login-form";
+import {Suspense} from "react";
 
 export default function LoginPage() {
 	return (
 		<div className="flex justify-center items-center min-h-screen">
-			<LoginForm />
+			<Suspense>
+				<LoginForm />
+			</Suspense>
 		</div>
 	);
 }
