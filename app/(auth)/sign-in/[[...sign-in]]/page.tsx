c4716a00bed5c64a15b1d1b1113d6992bca1fe41
Vadym Tatarinov <utikpuhlik@protonmail.ch>
2025-07-21 08:38:50 +0200

    feat(UX): add suspense to login form

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,16 @@
 import { StarsBackground } from "@/components/ui/stars-background";
-import {SignIn} from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
+import { Suspense } from "react";
+import { Loader } from "lucide-react";
 
 export default function Page() {
-
-    return (
-        <StarsBackground className="flex aspect-16/9 items-center justify-center">
-            <div className="h-screen flex items-center justify-center">
-                <SignIn/>
-            </div>
-        </StarsBackground>
-    )
+  return (
+    <StarsBackground className="flex aspect-16/9 items-center justify-center">
+      <div className="h-screen flex items-center justify-center">
+        <Suspense fallback={<Loader />}>
+          <SignIn />
+        </Suspense>
+      </div>
+    </StarsBackground>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,12 +1,16 @@
-import {SignUp} from '@clerk/nextjs'
-import {StarsBackground} from "@/components/ui/stars-background";
+import { SignUp } from "@clerk/nextjs";
+import { StarsBackground } from "@/components/ui/stars-background";
+import { Loader } from "lucide-react";
+import { Suspense } from "react";
 
 export default function Page() {
-    return (
-        <StarsBackground className="flex aspect-16/9 items-center justify-center">
-            <div className="h-screen flex items-center justify-center">
-                <SignUp/>
-            </div>
-        </StarsBackground>
-    )
+  return (
+    <StarsBackground className="flex aspect-16/9 items-center justify-center">
+      <div className="h-screen flex items-center justify-center">
+        <Suspense fallback={<Loader />}>
+          <SignUp />
+        </Suspense>
+      </div>
+    </StarsBackground>
+  );
 }
