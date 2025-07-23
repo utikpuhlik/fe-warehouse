a8d7b8510ab1712d4942f44535ce3f20f11f0b39
Vadym Tatarinov <utikpuhlik@protonmail.ch>
2025-07-23 19:15:25 +0200

    fix(User): rename notes -> note

--- app/lib/schemas/userSchema.ts
+++ app/lib/schemas/userSchema.ts
@@ -1,22 +1,26 @@
-import {z} from "zod";
+import { z } from "zod";
 
 /** Single user */
 export const zUserReadSchema = z.object({
-    id: z.string().uuid(),
-    clerk_id: z.string(),
-    first_name: z.string(),
-    last_name: z.string(),
-    email: z.string().email(),
-    is_active: z.boolean(),
-    role: z.enum(["ADMIN", "EMPLOYEE", "USER"]),
-    customer_type: z.enum(["USER_RETAIL", "USER_WHOLESALE", "USER_SUPER_WHOLESALE"]),
-    mailing: z.boolean(),
-    phone: z.string().nullable(),
-    city: z.string().nullable(),
-    notes: z.string().nullable(),
-    shipping_method: z.enum(["SELF_PICKUP", "CARGO", "OTHER"]).nullable(),
-    shipping_company: z.string().nullable(),
-    // addresses: z.array(zAddressSchema),
+  id: z.string().uuid(),
+  clerk_id: z.string(),
+  first_name: z.string(),
+  last_name: z.string(),
+  email: z.string().email(),
+  is_active: z.boolean(),
+  role: z.enum(["ADMIN", "EMPLOYEE", "USER"]),
+  customer_type: z.enum([
+    "USER_RETAIL",
+    "USER_WHOLESALE",
+    "USER_SUPER_WHOLESALE",
+  ]),
+  mailing: z.boolean(),
+  phone: z.string().nullable(),
+  city: z.string().nullable(),
+  note: z.string().nullable(),
+  shipping_method: z.enum(["SELF_PICKUP", "CARGO", "OTHER"]).nullable(),
+  shipping_company: z.string().nullable(),
+  // addresses: z.array(zAddressSchema),
 });
 
 /** Array wrapper for list endpoints */
