6c08519d45586186fea86b3e6cea2638c4bb433f
Vadym Tatarinov <utikpuhlik@protonmail.ch>
2025-07-25 12:42:12 +0200

    feat(design): improve color of breadcrumbs

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -3,37 +3,39 @@ import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 interface Breadcrumb {
-	label: string;
-	href: string;
-	active?: boolean;
+  label: string;
+  href: string;
+  active?: boolean;
 }
 
 export default function Breadcrumbs({
-                                        breadcrumbs,
-                                    }: {
-    breadcrumbs: Breadcrumb[];
+  breadcrumbs,
+}: {
+  breadcrumbs: Breadcrumb[];
 }) {
-    return (
-        <nav aria-label="Breadcrumb" className="mb-4 block">
-            <ol className="flex items-center text-base text-muted-foreground">
-                {breadcrumbs.map((breadcrumb, index) => (
-                    <li key={breadcrumb.href} className="flex items-center">
-                        <Link
-                            href={breadcrumb.href}
-                            aria-current={breadcrumb.active ? "page" : undefined}
-                            className={clsx(
-                                "transition-colors",
-                                breadcrumb.active ? "text-gray-900" : "text-gray-500 hover:text-gray-700"
-                            )}
-                        >
-                            {breadcrumb.label}
-                        </Link>
-                        {index < breadcrumbs.length - 1 && (
-                            <ChevronRight className="mx-2 h-5 w-5 text-muted-foreground" />
-                        )}
-                    </li>
-                ))}
-            </ol>
-        </nav>
-    );
+  return (
+    <nav aria-label="Breadcrumb" className="mb-4 block">
+      <ol className="flex items-center text-base text-muted-foreground">
+        {breadcrumbs.map((breadcrumb, index) => (
+          <li key={breadcrumb.href} className="flex items-center">
+            <Link
+              href={breadcrumb.href}
+              aria-current={breadcrumb.active ? "page" : undefined}
+              className={clsx(
+                "transition-colors",
+                breadcrumb.active
+                  ? "font-medium text-foreground"
+                  : "text-muted-foreground hover:text-primary",
+              )}
+            >
+              {breadcrumb.label}
+            </Link>
+            {index < breadcrumbs.length - 1 && (
+              <ChevronRight className="mx-1 h-5 w-5 text-muted-foreground" />
+            )}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
 }
--- app/ui/shared/sidenav.tsx
+++ app/ui/shared/sidenav.tsx
@@ -5,26 +5,26 @@ import Logo from "@/app/ui/shared/logo";
 import { ModeToggle } from "@/app/shared/mode-toggle";
 
 export default function SideNav() {
-    return (
-        <aside className="flex h-full flex-col justify-between border-r bg-background p-2 text-foreground">
-            <div>
-                <Link
-                    className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-900 p-2 md:h-40"
-                    href="/public"
-                >
-                    <div className="w-32 text-white md:w-40">
-                        <Logo />
-                    </div>
-                </Link>
-                <NavLinks />
-            </div>
+  return (
+    <aside className="flex h-full flex-col justify-between border-r bg-background p-2 text-foreground">
+      <div>
+        <Link
+          className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-900 p-2 md:h-40"
+          href="/public"
+        >
+          <div className="w-32 text-white md:w-40">
+            <Logo />
+          </div>
+        </Link>
+        <NavLinks />
+      </div>
 
-            <SignedIn>
-                <div className="flex items-center justify-between gap-2 mt-4">
-                    <ModeToggle />
-                    <UserButton showName />
-                </div>
-            </SignedIn>
-        </aside>
-    );
+      <SignedIn>
+        <div className="flex items-center justify-between gap-2 mt-4">
+          <ModeToggle />
+          <UserButton showName />
+        </div>
+      </SignedIn>
+    </aside>
+  );
 }
