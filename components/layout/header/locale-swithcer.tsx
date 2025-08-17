6aa8ab38897d28d70236297d89b03ef453f6760b
Vadym Tatarinov <utikpuhlik@protonmail.ch>
2025-08-17 19:22:47 +0200

    feat(i18n): add Turkish

--- components/layout/header/locale-swithcer.tsx
+++ components/layout/header/locale-swithcer.tsx
@@ -1,24 +1,28 @@
-import {useLocale, useTranslations} from 'next-intl';
+import { useLocale, useTranslations } from "next-intl";
 import LocaleSwitcherSelect from "@/components/layout/locale-switcher-select";
 
 export default function LocaleSwitcher() {
-    const t = useTranslations('LocaleSwitcher');
-    const locale = useLocale();
+  const t = useTranslations("LocaleSwitcher");
+  const locale = useLocale();
 
-    return (
-        <LocaleSwitcherSelect
-            defaultValue={locale}
-            items={[
-                {
-                    value: 'en',
-                    label: t('en')
-                },
-                {
-                    value: 'ru',
-                    label: t('ru')
-                }
-            ]}
-            label={t('label')}
-        />
-    );
+  return (
+    <LocaleSwitcherSelect
+      defaultValue={locale}
+      items={[
+        {
+          value: "en",
+          label: t("en"),
+        },
+        {
+          value: "ru",
+          label: t("ru"),
+        },
+        {
+          value: "tr",
+          label: t("tr"),
+        },
+      ]}
+      label={t("label")}
+    />
+  );
 }
--- i18n/config.ts
+++ i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number];
 
-export const locales = ['en', 'ru'] as const;
-export const defaultLocale: Locale = 'ru';
+export const locales = ["en", "ru", "tr"] as const;
+export const defaultLocale: Locale = "ru";
